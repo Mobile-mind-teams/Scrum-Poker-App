a05b0928a2e89bc3586aef940278e6dc0f293b16
cdmejiam94 <cdmejiam94@gmail.com>
2022-01-19 20:43:31 -0600

    Se agrego deep link para acceder a la app desde correo

--- app/src/main/java/com/example/scrumpokerapp/SplashActivity.kt
+++ app/src/main/java/com/example/scrumpokerapp/SplashActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.example.scrumpokerapp.view.activity.LoginActivity
 import com.example.scrumpokerapp.view.activity.MainActivity
 
 class SplashActivity : AppCompatActivity() {
